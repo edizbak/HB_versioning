f683a61953c3002b1925b47c4409d137773d7a1d
Jonathan <valla_jo@hotmail.com>
2019-07-04 15:18:37 +0200

    test5
    test6
    
    test7
    
    test8
    
    test9

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 console.log("start");
-function test2(x) {
-  if (x === 2) {
+function test(x) {
+  if (x === 3) {
     x = 0;
   }
   return x;
 }
-const a = test2(1);
-const b = test2(2);
+const a = test2(5);
+const b = test2(6);
 const c = test2(3);
 console.log("end", a, b, c);
